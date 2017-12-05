a54b55bc4f2590fbbd2485be08eb1eef406f1e11
Gil Barbara <gilbarbara@gmail.com>
2017-12-05 17:08:34 -0200

    fix build refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Payment from 'payment';
-import './styles.scss';
 
 class ReactCreditCards extends React.Component {
   constructor(props) {
