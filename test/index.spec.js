4b6e730aa468420409b4058abcd708dd15f1c7e9
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-04-07 13:47:37 -0300

    fix: technical adjustment

--- test/index.spec.js
+++ test/index.spec.js
@@ -251,7 +251,7 @@ describe('ReactCreditCards', () => {
     expect(wrapper.find('.rccs__number').hasClass('rccs--focused')).toBe(true);
 
     expect(mockCallback.mock.calls[0][0]).toEqual({ maxLength: 19, issuer: 'visa' });
-    expect(mockCallback.mock.calls[0][1]).toEqual(false);
+    expect(mockCallback.mock.calls[0][1]).toEqual(true);
   });
 
   it('should handle new number props (VisaElectron)', () => {
